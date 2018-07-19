f05ee1a991d1eff716f542ead2aa9dc035a9093c
CN-Tower <cntower@yahoo.com>
2018-07-19 21:34:18 +0800

    Update main.js

--- myresume/js/main.js
+++ myresume/js/main.js
@@ -1,74 +1,98 @@
-function indexLoaded(){
-/*==================================================================
-    获取一元素和声明变量
-  ==================================================================*/
-var $toInfo = $("#nav .toInfo"),
-    $infoQr = $("#nav .infoQr"),
-    $photo = $(".photo"),
-    $prompt = $("#section-1 .prompt"),
-    $project = $("#section-4 .projectBox dl"),
-    $pdt = $project.find("dt"),
-    $pdd = $project.find("dd"),
-    $em = $("#section-4 .projectBox dt em"),
-    $conns = $("#section-5 .connBox li"),
-    promptY,promptX;
-/*==================================================================
-    基本操作
-  ==================================================================*/
-    $toInfo.mouseenter(function(e){
-        $infoQr.stop(true,true).fadeIn();
-        $(this).on("mouseleave",function(){
-            $infoQr.stop(true,true).fadeOut()
-        });
-    }).click(function(){
-        $infoQr.stop(true,true).fadeOut();
-        $(this).off("mouseleave",function(){
-            $infoQr.stop(true,true).fadeOut()
-        });
-    });
-/*==================================================================
-    init or invoke plugins
-  ==================================================================*/
-    $('#nav').onePageNav();
-    //使用jquery.scrollTo.js
-    $(".logo").click(function(){
-        $.scrollTo('#section-1',1000);
-    });
-    $(".btn-con:even").click(function(){
-        $.scrollTo('#section-5',1000);
-    });
-    $(".btn-con:odd").click(function(){
-        $.scrollTo('#section-1',1000);});
-    //初始化wow.js
-    if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){
+function indexLoaded() {
+    /**
+     * 初始化wow.js
+     */
+    if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))) {
         new WOW().init();
     }
-/*==================================================================
-    prompt的鼠标跟踪
-  ==================================================================*/
-    $photo.hover(function(){
-        $prompt.stop(true,true).fadeIn();
-    },function(){
-        $prompt.stop(true,true).fadeOut();
-    }).mousemove(function(e){
+
+    /**
+     * 滑动到指定区域
+     */
+    $(document).scrollTop(0);
+    $(".logo").click(function () {
+        scrollToSec('#section-1', 1000);
+    });
+    $(".btn-con:even").click(function () {
+        scrollToSec('#section-5', 1000);
+    });
+    $(".btn-con:odd").click(function () {
+        scrollToSec('#section-1', 1000);
+    });
+    $('#nav li a').click(function() {
+        scrollToSec($(this).attr("href"));
+        return false;
+    });
+
+    function scrollToSec(secId) {
+        $('.topnav li a').each(function() {
+            if ($(this).attr("href") === secId) {
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+        });
+        $("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: $(secId).offset().top
+        }, 1000 );
+    }
+
+    /**
+     * 个人信息二维码
+     */
+    var $toInfo = $("#nav .toInfo")
+        ,$infoQr = $("#nav .infoQr");
+
+    $toInfo.mouseenter(function (e) {
+        $infoQr.stop(true, true).fadeIn();
+        $(this).on("mouseleave", function () {
+            $infoQr.stop(true, true).fadeOut()
+        });
+    }).click(function () {
+        $infoQr.stop(true, true).fadeOut();
+        $(this).off("mouseleave", function () {
+            $infoQr.stop(true, true).fadeOut()
+        });
+    });
+
+    /**
+     * 头像上的弹出框提示
+     */
+    var $photo = $(".photo")
+        ,$prompt = $("#section-1 .prompt")
+        ,promptY, promptX;
+
+    $photo.hover(function () {
+        $prompt.stop(true, true).fadeIn();
+    }, function () {
+        $prompt.stop(true, true).fadeOut();
+    }).mousemove(function (e) {
         promptY = e.pageY;
         promptX = e.pageX;
-        promptSY= $(document).scrollTop();
-        promptSX= $(document).scrollLeft();
+        promptSY = $(document).scrollTop();
+        promptSX = $(document).scrollLeft();
         $prompt.css({
-            top:promptY-promptSY+30,
-            left:promptX-promptSX-25
+            top: promptY - promptSY + 30,
+            left: promptX - promptSX - 25
         })
     });
-/*==================================================================
-    项目经验的抽屉效果
-  ==================================================================*/
-    $pdt.each(function(i){
-        $(this).click(function(){
-            $em.css({transform:"rotate(0deg)"})
-                .eq(i).css({transform:"rotate(-90deg)"});
-            $pdd.stop().animate({width:"0"})
-                .eq(i).stop().animate({width:"386px"})
+
+    /**
+     * 项目经验的抽屉效果
+     */
+    var $project = $("#section-4 .projectBox dl")
+        ,$pdt = $project.find("dt")
+        ,$pdd = $project.find("dd")
+        ,$em = $("#section-4 .projectBox dt em")
+
+    $pdt.each(function (i) {
+        $(this).click(function () {
+            $em
+                .css({ transform: "rotate(0deg)" })
+                .eq(i).css({ transform: "rotate(-90deg)" });
+            $pdd
+                .stop().animate({ width: "0" })
+                .eq(i).stop().animate({ width: "386px" })
         })
     });
 }
