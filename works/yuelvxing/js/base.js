b35f4b034cabbc38c96c4276dfb3c14a7b935181
CN-Tower <247114045@qq.com>
2018-10-25 13:07:55 +0800

    Update

--- works/yuelvxing/js/base.js
+++
@@ -1,8 +0,0 @@
-
-/* public variable */
-
-var requirePath = {
-    CONFIG: '/js/config.js?v=' + new Date().getTime(),
-    JSVERSION: '_v=' + new Date().getTime(),
-    ROOTURL: '/'
-};
--- works/yuelvxing/static/js/base.js
+++
@@ -1,7 +0,0 @@
-/* public variable */
-
-var requirePath = {
-    CONFIG: '/static/js/config.js?v=' + new Date().getTime(),
-    JSVERSION: '_v=' + new Date().getTime(),
-    ROOTURL: '/'
-};
